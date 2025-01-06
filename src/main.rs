869eefca8cda268d4872c494e2e3f3f113e187ef
ｳﾀｶﾀｷｮｳｽｲ <c1101081@planet.kanazawa-it.ac.jp>
2025-01-06 18:49:43 +0900

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use state::WgpuState;
 use winit::{application::ApplicationHandler, error::EventLoopError, event::{ElementState, WindowEvent}, event_loop::{ControlFlow, EventLoop}, keyboard::{KeyCode, PhysicalKey}, window::Window};
 use std::result::Result;
 
-
 #[derive(Default)]
 pub struct App<'a> {
     window: Option<Arc<Window>>,
@@ -14,16 +13,30 @@ pub struct App<'a> {
 impl<'a> ApplicationHandler for App<'a> {
     fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
         if self.window.is_none() {
-            let window_attr = Window::default_attributes()
-                .with_title("Pentagrams");
-            let window = Arc::new(
-                event_loop
-                    .create_window(window_attr)
-                    .expect("Failed to create window"),
-            );
-            self.window = Some(window.clone());
-
-            let wgpu_state = WgpuState::new(window.clone());
+            #[cfg(target_arch = "wasm32")]
+            {
+                use winit::platform::web::WindowBuilderExtWebSys;
+                use winit::platform::web::WindowAttributesExtWebSys;
+                let window_attr = Window::default_attributes()
+                    .with_title("Pentagrams in WebAssembly");
+                let window = Arc::new(
+                    event_loop
+                        .create_window(window_attr)
+                        .expect("Failed to create window"),
+                );
+                self.window = Some(window.clone());
+            }
+            #[cfg(not(target_arch = "wasm32"))] {
+                let window_attr = Window::default_attributes()
+                    .with_title("Pentagrams");
+                let window = Arc::new(
+                    event_loop
+                        .create_window(window_attr)
+                        .expect("Failed to create window"),
+                );
+                self.window = Some(window.clone());
+            }
+            let wgpu_state = WgpuState::new(self.window.clone().unwrap());
             self.wgpu_state = Some(wgpu_state);
         }
     }
@@ -78,6 +91,7 @@ fn main() -> Result<(), EventLoopError> {
         .set_control_flow(ControlFlow::Poll);
 
     let mut app = App::default();
+    
     let run_app =  event_loop.run_app(&mut app);
     run_app
 }
